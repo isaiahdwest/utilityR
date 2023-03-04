23c02b69291a51a834baf7dadee32d19a097e5b4
Isaiah Westphalen <iwestphalen@guidehouse.com>
2023-03-04 16:22:42 -0500

    adding as setClass/method

--- R/fx_data_man.R
+++ R/fx_data_man.R
@@ -142,9 +142,23 @@ map_fns <- function(.x, .fns, error_handling = "do-nothing") {
     do.call('<-', list(l[[II]], r[[II]]), envir=Envir)
   }
 }
+setClass("lbunch")
+setGeneric("%=%")
+setMethod("%=%", "lbunch", function(l, r, ...) {
+  Envir = as.environment(-1)
 
-`%=%` = `%=%.lbunch`
+  if (length(r) > length(l))
+    warning("RHS has more args than LHS. Only first", length(l), "used.")
 
+  if (length(l) > length(r))  {
+    warning("LHS has more args than RHS. RHS will be repeated.")
+    r <- extendToMatch(r, l)
+  }
+
+  for (II in 1:length(l)) {
+    do.call('<-', list(l[[II]], r[[II]]), envir=Envir)
+  }
+})
 
 # Used if LHS is larger than RHS
 #'@export

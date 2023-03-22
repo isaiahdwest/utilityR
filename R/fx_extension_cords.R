8ac3013904a46d61b0ddb5a1b2af874cc28b3d51
isaiahdwest <isaiahwestphalen@gmail.com>
2023-03-22 14:12:27 -0400

    rm_ex

--- R/fx_extension_cords.R
+++ R/fx_extension_cords.R
@@ -13,3 +13,24 @@ split2 <- function(x, f, drop = FALSE, nm = NULL, ...) {
   }
   out
 }
+
+#' @title remove objects from environment
+#' @description Remove all objects from environment EXCEPT
+#' those that are specified.
+#' @param ... what objects to retain in environment
+#' @param envir Which environment to remove objects from, default is the global environment
+#' @examples \dontrun {
+#' a <- "TEST"
+#' b <- "TEST2"
+#'
+#' rm_ex(a)
+#' }
+#' @export
+rm_ex <- function(..., envir = globalenv()) {
+
+  objs <- match.call(expand.dots = FALSE)$...
+
+  enVars <- ls(envir = envir)
+
+  rm(list = setdiff(enVars, unlist(objs)), envir = envir)
+}
